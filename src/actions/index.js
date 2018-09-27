3d204bacc2398567f71e1a1d658691bbf048c415
Devin Mounts <devinmounts@gmail.com>
2018-09-27 10:38:48 -0700

    create action types for requesting and receiving sources.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,5 +2,18 @@ import * as types from './../constants/ActionTypes';
 import v4 from 'uuid';
 
 export function fetchSources(){
-  return 
+  return (dispatch) {
+    const localSearchId = v4();
+    dispatch(requestSources())
+    return fetch('https://newsapi.org/v2/sources?apiKey=72b782e18f474153aa0e481445dec308').then(
+      response => response.json(),
+      error => console.log('An error occured', error)
+    ).then(function(json) {
+      console.log(json);
+    })
+  }
 }
+
+export const requestSources = () => ({
+  type: types.REQUEST_SOURCES
+});
