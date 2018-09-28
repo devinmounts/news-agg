d00be34cbad2c8c86d27237448c6541f9c5d0b97
Devin Mounts <devinmounts@gmail.com>
2018-09-28 14:32:18 -0700

    display article content in DOM.

---
+++ src/components/Article.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { updateCurrentSourceUrl } from './../actions';
+
+function Article(props) {
+  const { title, author, source, description, url, image, dispatch } = props;
+
+  return(
+    <div>
+      <img src={image} />
+      <h1>{title}</h1>
+      <h5>{author}</h5>
+      <p>{description}</p>
+      <h5><a href={url}>Read More at {source}</a></h5>
+    </div>
+  );
+}
+
+Article.propTypes = {
+  name: PropTypes.string,
+  url: PropTypes.string,
+  key: PropTypes.string
+};
+export default connect()(Article);
--- src/components/DisplayContainer.jsx
+++ src/components/DisplayContainer.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Article from './Article';
 
-function DisplayContainer() {
-  return (
-    <div>
-      Container Works
-    </div>
-  );
+class DisplayContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+  }
+  render(){
+    console.log(this.props)
+    let articles = null;
+
+    if (this.props.articlesObject.articles != undefined){
+      articles = Object.keys(this.props.articlesObject.articles).map((articleId) => {
+        let article = this.props.articlesObject.articles[articleId];
+        return <Article
+          title = {article.title}
+          author = {article.author}
+          source = {article.source.name}
+          description = {article.description}
+          url = {article.url}
+          image = {article.urlToImage}
+          key = {articleId} />
+      });
+    }
+    return (
+      <div>
+        {articles !=null ? articles : ''}
+      </div>
+    );
+  }
 }
 
 export default DisplayContainer;
