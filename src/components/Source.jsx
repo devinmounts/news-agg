4b0e31db861010c95937a2af56fe414e7c21ad35
Devin Mounts <devinmounts@gmail.com>
2018-09-28 09:48:48 -0700

    display news sources on DOM.

--- src/components/Source.jsx
+++ src/components/Source.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Source(props) {
+  const { name, url } = props;
   return(
     <div>
-      Source Works
+      <span key={url}>{name}</span>
     </div>
   );
 }
 
+Source.propTypes = {
+  name: PropTypes.string,
+  url: PropTypes.string,
+  key: PropTypes.string
+}
 export default Source;
