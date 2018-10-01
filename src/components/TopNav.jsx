953d500becfd5ce7227cebcb24239b3837338857
Devin Mounts <devinmounts@gmail.com>
2018-10-01 16:46:03 -0700

    end of monday.

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -23,7 +23,7 @@ class TopNav extends React.Component {
         <SearchBar />
         <div className='flex-box' >
           <span onClick={(e) => this.activateTab(e.target.attributes[0].value)} value='top-headlines' className={this.state.activeTab === 'top-headlines' ? 'activeTab' : 'category'}>Top Headlines</span>
-          <span onClick={(e) => this.activateTab(e.target.attributes[0].value)} value='everything' className={this.state.activeTab === 'top-headlines' ? 'activeTab' : 'category'}>All Articles</span>
+          <span onClick={(e) => this.activateTab(e.target.attributes[0].value)} value='everything' className={this.state.activeTab === 'everything' ? 'activeTab' : 'category'}>All Articles</span>
         </div>
       </div>
 
