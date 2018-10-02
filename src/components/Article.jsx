e7c46ece2ae22236d914521c93cd5ed4611ab0a8
Student <example@example.com>
2018-10-02 10:21:59 -0700

    add toggle display for article onMouseEnter/Leave.

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,19 +4,45 @@ import { connect } from 'react-redux';
 import { updateCurrentSourceUrl } from './../actions';
 import './styles/Article.css';
 
-function Article(props) {
-  const { title, author, source, description, url, image, dispatch } = props;
+class Article extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      readMoreVisible: false
+    };
+    this.handleReadMoreVisible = this.handleReadMoreVisible.bind(this);
+  }
 
-  return(
-    <div className='article-container'>
-      <h5 className='article-source'>Source: {source}</h5>
-      <a href={url}><img className='image' src={image} /></a>
-      <h1>{title}</h1>
-      <h5>{author}</h5>
-      <p>{description}</p>
 
-    </div>
-  );
+  handleReadMoreVisible(){
+    this.setState({
+      readMoreVisible: !this.state.readMoreVisible
+    });
+  }
+
+  render(){
+    const { title, author, source, description, url, image, dispatch } = this.props;
+    let sourceDisplay = null;
+    let titleDisplay = null;
+    if(this.state.readMoreVisible){
+      sourceDisplay = <h5 className='read-source'>READ MORE</h5>
+      titleDisplay = <h1 className='read-title'>{title}</h1>
+    } else {
+      sourceDisplay = <h5 className='article-source'>Source: {source}</h5>
+      titleDisplay = <h1>{title}</h1>
+
+    }
+
+    return(
+      <div className='article-container'>
+        {sourceDisplay}
+        <a href={url}><img onMouseEnter={this.handleReadMoreVisible} onMouseLeave={this.handleReadMoreVisible} className='image' src={image} /></a>
+        {titleDisplay}
+        <h5>{author}</h5>
+        <p>{description}</p>
+      </div>
+    );
+  }
 }
 
 Article.propTypes = {
