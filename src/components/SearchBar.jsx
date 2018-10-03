0efa7ccc932755d11ec58101eb3f642dc1acf268
Student <example@example.com>
2018-10-03 15:16:34 -0700

    retreive info from expansion.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,27 +5,48 @@ import { fetchArticlesByUserSearch } from './../actions';
 import SearchExpansion from './SearchExpansion';
 import { connect } from 'react-redux';
 
-function SearchBar({dispatch}) {
-  let input;
-  return(
-    <div className='searchDiv'>
-      <h1 className='header'>Search global news.</h1>
-      <form onSubmit={e => {
-        e.preventDefault();
-        if (!input.value.trim()) {
-          return;
-        }
-        dispatch(fetchArticlesByUserSearch(input.value.trim()));
-      }}>
-        <input className='input'
-          ref={node => {input = node;}}
-          placeholder="e.g. Economic Analysis"
-        />
-        <button className='button' type='submit'><img className='icon'src={search}/></button>
-      </form>
-      <SearchExpansion />
-    </div>
-  );
+class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+    let text = null;
+    let dateFrom = null;
+    let dateTo = null;
+    this.handleDateFrom = this.handleDateFrom.bind(this);
+    this.handleDateTo = this.handleDateTo.bind(this);
+  }
+
+
+  handleDateFrom(selectedDateFrom){
+    this.dateFrom = selectedDateFrom
+  }
+
+  handleDateTo(selectedDateTo){
+    this.dateTo = selectedDateTo
+  }
+
+  render(){
+    const { dispatch } = props
+    return(
+      <div className='searchDiv'>
+        <h1 className='header'>Search global news.</h1>
+        <form onSubmit={e => {
+          e.preventDefault();
+          if (!this.text.value.trim()) {
+            return;
+          }
+          dispatch(fetchArticlesByUserSearch(this.text.value.trim(), this.dateFrom, this.dateTo));
+        }}>
+          <input className='input'
+            ref={node => {this.text = node;}}
+            placeholder="e.g. Economic Analysis"
+          />
+          <button className='button' type='submit'><img className='icon'src={search}/></button>
+        </form>
+        <SearchExpansion onHandleDateFrom={this.handleDateFrom} onHandleDateTo={this.handleDateTo} />
+      </div>
+    );
+  }
 }
+
 
 export default connect()(SearchBar);
--- src/components/SearchExpansion.jsx
+++ src/components/SearchExpansion.jsx
@@ -1,24 +1,49 @@
 import React from 'react';
 import './styles/SearchExpansion.css';
+import PropTypes from 'prop-types';
 
 function SearchExpansion(props) {
-
-    function handleFilterCategory(){
-
-    }
-
-    return (
-      <div className='search-expansion' >
-        <div className='date-div'>
-          <span>From: </span><input type='date' /><br/>
-          <span>To:</span><input className='to-date' type='date' />
-        </div>
-
-
-        <button className="buttonStyle"  value="city" onClick={handleFilterCategory}>City</button>
-        <p className='inline-p'>Filter Options</p>
-      </div>
-    )
+  console.log(props)
+  let dateFrom;
+  let dateTo;
+  function handleFromDate(e){
+    dateFrom = e.target.value;
+    props.onHandleDateFrom(dateFrom);
   }
+
+  function handleToDate(e){
+     dateTo = e.target.value
+    props.onHandleDateTo(dateTo);
+  }
+
+
+
+
+  return (
+    <div className='search-expansion' >
+      <div className='date-div'>
+        <span>From: </span><input onChange={handleFromDate} ref={node => {dateFrom = node}} type='date' /><br/>
+        <span>To:</span><input onChange={handleToDate} ref={node => {dateTo = node}} className='to-date' type='date' />
+      </div>
+      <div>
+        <select >
+          <option>Sort By</option>
+          <option value='relevancy'>Most Relevant</option>
+          <option value='popularity'>Popular Sources</option>
+          <option value='publishedAt'>Most Recent</option>
+        </select>
+      </div>
+
+
+      <button className="buttonStyle"  value="city" >City</button>
+      <p className='inline-p'>Filter Options</p>
+    </div>
+  )
+}
+
+SearchExpansion.propTypes = {
+  onHandleDateFrom: PropTypes.func,
+  onHandleDateTo: PropTypes.func
+}
 
 export default SearchExpansion;
