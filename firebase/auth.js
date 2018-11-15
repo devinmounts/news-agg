f5ecb7aca2b37157e89e48b68f991a1e4740e3ed
Devin Mounts <devinmounts@gmail.com>
2018-11-14 16:12:53 -0800

    export firebase files from  index.js.

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,1 +1,21 @@
 import { auth } from './firebase';
+
+//Sign Up
+export const doCreateUserWithEmailAndPassword = (email, password) =>
+    auth.doCreateUserWithEmailAndPassword(email, password);
+
+//Sign In
+export const doSignInWithEmailAndPassword = (email, password) =>
+    auth.doSignInWithEmailAndPassword(email, password);
+
+//Sign Out
+export const doSignOut = () =>
+    auth.signOut();
+
+//Password Reset
+export const doPasswordReset = (email) =>
+    auth.sendPasswordResetEmail(email);
+
+//Password Change
+export const doPasswordUpdate = (password) => 
+    auth.currentUser.updatePassword(password);
