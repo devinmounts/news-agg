cdf742128d873b3cb9ffe6206d2dd31208543949
Devin Mounts <devinmounts@gmail.com>
2018-12-04 13:40:12 -0800

    create auth methods, remove firebase dir.

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,3 @@
-// import firebase from 'firebase/app';
 import app from 'firebase/app';
 import 'firebase/auth';
 
@@ -26,7 +25,25 @@ const config =
 class Firebase {
 	constructor() {
 		app.initilizeApp(config);
+
+		this.auth = app.auth();
 	}
+
+	// *** Auth Api ***
+
+	doCreateUserWithEmailAndPassword = (email, password) =>
+	this.auth.doCreateUserWithEmailAndPassword(email, password);
+	
+	doSignInWithEmailAndPassword = (email, password) =>
+	this.auth.doSignInWithEmailAndPassword(email, password);
+	
+	doSignOut = () => this.auth.signOut();
+
+	doPasswordReset = (email) =>
+	this.auth.sendPasswordResetEmail(email);
+	
+	doPasswordUpdate = (password) => 
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
